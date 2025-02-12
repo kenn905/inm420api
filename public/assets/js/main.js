b0f4211c6eb62874d570e873adf7ce1d69b11bdc
kenn905 <kenn.p905@gmail.com>
2025-02-11 23:29:28 -0500

    final

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,6 +1,39 @@
-console.log('test this');
+async function getData() {
+    const word = document.getElementById("search").value.trim();
+    // error if no word input
+    if (!word) {
+        alert("Please enter a word!");
+        return;
+    }
 
-async function getData(params) {
-    const response =await fetch('http://api.weatherapi.com/v1/current.json?key=5ab27e3a225e4ee6a00162202252901');
+    var API_KEY = "0c7251f3-1c94-4d73-ba7b-18f2c9f06fc8"; 
+    var URL = `https://www.dictionaryapi.com/api/v3/references/collegiate/json/${word}?key=${API_KEY}`;
+
+    try {
+        const response = await fetch(URL);
+        // console.log(response)
+        const data = await response.json();
+        // console.log(data)
+
+        // get results div
+        const resultDiv = document.getElementById("result");
+        // reset results
+        resultDiv.innerHTML = ""; 
+
+        // place data from json in variable
+        // check if data is array & not empty
+        if (Array.isArray(data) && data.length > 0 && data[0].shortdef) {
+            // get word & definition
+            const wordContent = `<div class="word">${word}</div>`;
+            const definitionContent = `<div class="definition"><b>Definition:</b> ${data[0].shortdef.join(", ")}</div>`;
+            // display results
+            resultDiv.innerHTML = wordContent + definitionContent;
+        } else {
+            // else no results found
+            resultDiv.innerHTML = `<p>No results found for "${word}".</p>`;
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        document.getElementById("result").innerHTML = "<p>Failed to fetch definition.</p>";
+    }
 }
-getData();
